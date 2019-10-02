80dcb43de8c7a28555ba95fed346b961138433fa
Vishal <vshlvrm232@gmail.com>
2019-10-03 00:24:10 +0530

    Updated Games.js with following changes: - Added Wall class to create walls.

--- Game.js
+++ Game.js
@@ -1,3 +1,63 @@
+/**
+ * This class is used to add walls to the world.
+ * The constructor of this class takes margin as the arguments which is used to set the
+ * margin of the walls.
+ *
+ * How to use:
+ *
+ * const wall = new Wall(100)
+ * wall.addWalls()
+ */
+class Wall {
+    constructor(margin) {
+        this.strokeSize = 10
+        this.walls = []
+        //Top and bottom walls
+        this.verticalWallsPositionX = window.innerWidth/2
+        this.topSidePostionY = this.strokeSize + margin
+        this.bottomSidePostionY = window.innerHeight - this.strokeSize - margin
+        this.width = window.innerWidth - this.strokeSize - margin*2;
+
+        //Left and Right walls
+        this.horizontalWallsPositionY = window.innerHeight/2
+        this.rightSidePositionX = window.innerWidth - this.strokeSize - margin
+        this.leftSidePositionX = this.strokeSize + margin
+        this.height = window.innerHeight - this.strokeSize - margin*2
+    }
+
+    addWalls() {
+        const bottomSide = Bodies.rectangle(
+            this.verticalWallsPositionX,
+            this.bottomSidePostionY,
+            this.width,
+            this.strokeSize,
+            { isStatic: true});
+        const topSide = Bodies.rectangle(
+            this.verticalWallsPositionX,
+            this.topSidePostionY,
+            this.width,
+            this.strokeSize,
+            { isStatic: true});
+        const leftSide = Bodies.rectangle(
+            this.leftSidePositionX,
+            this.horizontalWallsPositionY,
+            this.strokeSize,
+            this.height,
+            { isStatic: true});
+        const rightSide = Bodies.rectangle(
+            this.rightSidePositionX,
+            this.horizontalWallsPositionY,
+            this.strokeSize,
+            this.height,
+            { isStatic: true});
+
+        this.walls.push(bottomSide)
+        this.walls.push(topSide)
+        this.walls.push(leftSide)
+        this.walls.push(rightSide)
+        World.add(engine.world, this.walls);
+    }
+}
 var Engine = Matter.Engine,
     Render = Matter.Render,
     World = Matter.World,
@@ -10,21 +70,15 @@ var render = Render.create({
     element: document.body,
     engine: engine,
     options: {
-        width:1400,
-        height:920,
+        width:window.innerWidth,
+        height:window.innerHeight,
         wireframes:false,
     }
 });
-var rest = 0.9, 
-        space = 600 / 5;
 var batA = Bodies.rectangle(400,200,30,200, { isStatic: true});
 var batB = Bodies.rectangle(1200,200,30,200, {isStatic: true});
 var ball = Bodies.circle(580,100,40,10);
 ball.restitution = 1.0
-var bottomSide = Bodies.rectangle(800, 900, 1024, 10, { isStatic: true});
-var topSide = Bodies.rectangle(800, 100, 1024, 10, { isStatic: true});
-var leftSide = Bodies.rectangle(290, 500, 10, 810, { isStatic: true});
-var rightSide = Bodies.rectangle(1310, 500, 10, 810, { isStatic: true});
 
 var downAndRotateLeft = {"ArrowDown":false,"ArrowLeft":false}
 var downAndRotateRight = {"ArrowDown":false,"ArrowRight":false}
@@ -32,7 +86,6 @@ var upAndRotateLeft = {"ArrowUp":false,"ArrowLeft":false}
 var upAndRotateRight = {"ArrowUp":false,"ArrowRight":false}
 var isKeyPressed = false
 
-Event.on(batA,"")
 document.onkeydown = function() {
     var keyPressed = window.event.key
     if (keyPressed in downAndRotateLeft) {
@@ -41,7 +94,6 @@ document.onkeydown = function() {
             Body.translate(batA, {x:0,y:15})
             Body.rotate(batA,-0.3)
             isKeyPressed = true
-            console.log("downAndRotateLeft"+batA.position.x+batA.position.y)
         }
     } 
     if (keyPressed in downAndRotateRight) {
@@ -50,7 +102,6 @@ document.onkeydown = function() {
             Body.translate(batA, {x:0,y:15})
             Body.rotate(batA,0.3)
             isKeyPressed = true
-            console.log("downAndRotateRight")
         }
     }
     if (keyPressed in upAndRotateLeft) {
@@ -59,7 +110,6 @@ document.onkeydown = function() {
             Body.translate(batA, {x:0,y:-15})
             Body.rotate(batA,-0.3)
             isKeyPressed = true
-            console.log("upAndRotateLeft")
         }
     }
     if (keyPressed in upAndRotateRight) {
@@ -68,25 +118,20 @@ document.onkeydown = function() {
             Body.translate(batA, {x:0,y:-15})
             Body.rotate(batA,0.3)
             isKeyPressed = true
-            console.log("upAndRotateRight")
         }
     }
     if (isKeyPressed == false) {
         switch(keyPressed) {
             case "ArrowUp":
-                console.log("up")
                 Body.translate(batA, {x:0,y:-15})
                 break
             case "ArrowDown":
-                console.log("Down")
                 Body.translate(batA, {x:0,y:15})
                 break
             case "ArrowLeft":
-                console.log("left")
                 Body.rotate(batA,-0.3)
                 break
             case "ArrowRight":
-                console.log("Right")
                 Body.rotate(batA,0.3)
                 break;
         }
@@ -103,7 +148,10 @@ document.onkeyup = function() {
     upAndRotateLeft["ArrowUp"] = false
     upAndRotateLeft["ArrowLeft"] = false
 }
-World.add(engine.world, [bottomSide, topSide, leftSide, rightSide]);
+//Add walls to the world
+const walls = new Wall(200)
+walls.addWalls()
+
 World.add(engine.world, batA);
 World.add(engine.world, batB);
 World.add(engine.world, ball);
